a38c4a51db7200864faa11cfba43c518709a70d7
WKD622 <kuba.ziarko@gmail.com>
2019-03-14 15:05:39 +0100

    fixes based on the last CR

--- api/views.py
+++ api/views.py
@@ -41,16 +41,20 @@ class EmployeeViewSet(viewsets.ModelViewSet):
     def fire(self, request, *args, **kwargs):
         employee = self.get_object()
         employee.hired = False
-        employee.save()
+        employee.save(update_fields=['hired'])
         serializer = EmployeeSerializer(employee, many=False)
+        serializer.is_valid(raise_exception=True)
+        self.perform_update(serializer)
         return Response(serializer.data)
 
     @action(detail=True, methods=['get'])
     def hire_again(self, request, *args, **kwargs):
         employee = self.get_object()
         employee.hired = True
-        employee.save()
+        employee.save(update_fields=['hired'])
         serializer = EmployeeSerializer(employee, many=False)
+        serializer.is_valid(raise_exception=True)
+        self.perform_update(serializer)
         return Response(serializer.data)
 
     @action(detail=True, methods=['post'])
@@ -61,15 +65,17 @@ class EmployeeViewSet(viewsets.ModelViewSet):
         if pesel is not None and new_position is not None:
             employee = get_object_or_404(Employee, pesel=pesel)
             employee.position = new_position
-            employee.save()
+            employee.save(update_fields=['position'])
             serializer = EmployeeSerializer(employee, many=False)
+            serializer.is_valid(raise_exception=True)
+            self.perform_update(serializer)
             return Response(serializer.data)
 
     @action(detail=False, methods=['get'])
     def summary_cost_of_salaries(self, request, *args, **kwargs):
-        sum_of_salaries = Employee.objects.all().aggregate(Sum('salary'))
-        print(sum_of_salaries)
-        return Response("Summary cost of all salaries: " + str(sum_of_salaries['salary__sum']))
+        result = Employee.objects.aggregate(sum_of_salaries=Sum('salary'))
+        print(result)
+        return Response("Summary cost of all salaries: " + str(result['sum_of_salaries']))
 
 
 class IncomeViewSet(viewsets.ModelViewSet):
@@ -79,21 +85,25 @@ class IncomeViewSet(viewsets.ModelViewSet):
 
     @action(detail=False, methods=['get'])
     def last_incomes(self, request, *args, **kwargs):
-        number_of_incomes_to_show = request.GET.get('number_of_incomes', None)
-        if number_of_incomes_to_show is not None:
-            incomes = Income.objects.order_by('date')[:int(number_of_incomes_to_show)]
-        else:
-            incomes = Income.objects.order_by('date')
+        number_of_incomes_to_show = request.GET.get('number_of_incomes')
+        incomes = Income.objects.order_by('date')
+        if number_of_incomes_to_show:
+            incomes = incomes[:int(number_of_incomes_to_show)]
+
         serializer = EventSerializer(incomes, many=True)
         return Response(serializer.data)
 
     @action(detail=False, methods=['get'])
-    def sum_of_cost(self, request, *args, **kwargs):
+    def sum(self, request, *args, **kwargs):
         days_back = request.GET.get('days', 7)
-        current_date = datetime.today().date()
-        date_for_filter = current_date - timedelta(days=int(days_back))
-        sum_of_incomes = Income.objects.filter(date__gte=date_for_filter).aggregate(Sum('sum'))
-        return Response("Total income from the last " + str(days_back) + " days is: " + str(sum_of_incomes['sum__sum']))
+        if str(days_back).isdigit():
+            current_date = datetime.today().date()
+            date_for_filter = current_date - timedelta(days=int(days_back))
+            sum_of_incomes = Income.objects.filter(date__gte=date_for_filter).aggregate(sum_of_incomes=Sum('sum'))['sum_of_incomes']
+            if not sum_of_incomes:
+                sum_of_incomes = "0"
+            return Response("Total income from the last " + str(days_back) + " days is: " + str(sum_of_incomes))
+        return Response("days parameter should be a number")
 
 
 class OutcomeViewSet(viewsets.ModelViewSet):
@@ -104,21 +114,24 @@ class OutcomeViewSet(viewsets.ModelViewSet):
     @action(detail=False, methods=['get'])
     def last_outcomes(self, request, *args, **kwargs):
         number_of_outcomes_to_show = request.GET.get('number_of_outcomes', None)
-        if number_of_outcomes_to_show is not None:
-            outcomes = Outcome.objects.order_by('date')[:int(number_of_outcomes_to_show)]
-        else:
-            outcomes = Outcome.objects.order_by('date')
+        outcomes = Outcome.objects.order_by('date')[:int(number_of_outcomes_to_show)]
+        if number_of_outcomes_to_show:
+            outcomes = outcomes[:int(number_of_outcomes_to_show)]
         serializer = EventSerializer(outcomes, many=True)
         return Response(serializer.data)
 
     @action(detail=False, methods=['get'])
-    def sum_of_cost(self, request, *args, **kwargs):
+    def sum(self, request, *args, **kwargs):
         days_back = request.GET.get('days', 7)
-        current_date = datetime.today().date()
-        date_for_filter = current_date - timedelta(days=int(days_back))
-        sum_of_outcomes = Outcome.objects.filter(date__gte=date_for_filter).aggregate(Sum('sum'))
-        return Response(
-            "Total outcome from the last " + str(days_back) + " days is: " + str(sum_of_outcomes['sum__sum']))
+        if str(days_back).isdigit():
+            current_date = datetime.today().date()
+            date_for_filter = current_date - timedelta(days=int(days_back))
+            sum_of_outcomes = Outcome.objects.filter(date__gte=date_for_filter).aggregate(sum_of_outcomes=Sum('sum'))['sum_of_outcomes']
+            if not sum_of_outcomes:
+                sum_of_outcomes = "0"
+            return Response(
+                "Total outcome from the last " + str(days_back) + " days is: " + str(sum_of_outcomes))
+        return Response("days parameter should be a number")
 
 
 class EventViewSet(viewsets.ModelViewSet):
