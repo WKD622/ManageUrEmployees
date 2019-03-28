6f4f815c374b8887ef683ee8684c9b4d385bb4e8
WKD622 <kuba.ziarko@gmail.com>
2019-03-28 14:07:28 +0100

    end event tests

--- api/tests/test_models.py
+++ api/tests/test_models.py
@@ -1,9 +1,7 @@
 import pytest
-
-from django.db.models import QuerySet
 from django.utils import timezone
 
-from ..models import Event, Employee
+from ..models import Event
 
 
 @pytest.mark.django_db
@@ -27,3 +25,22 @@ def test_future_events():
 
     # then
     assert manager_events == future_events
+
+
+@pytest.mark.django_db
+def test_todays_events():
+    # given
+    today = timezone.now()
+
+    past_event = Event(name='name1', description='description1', datetime=today - timezone.timedelta(days=3))
+    todays_event = Event(name='name2', description='description2', datetime=today)
+    future_event = Event(name='name3', description='description3', datetime=today + timezone.timedelta(days=2))
+    past_event.save()
+    todays_event.save()
+    future_event.save()
+
+    # when
+    manager_events = list(Event.objects.todays_events())
+
+    # then
+    assert manager_events == [todays_event]
