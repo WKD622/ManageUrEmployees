4f5ae6d6783c6f731302774e222343678114915f
WKD622 <kuba.ziarko@gmail.com>
2019-04-04 12:09:11 +0200

    views test impr

--- api/tests/views/test_events.py
+++ api/tests/views/test_events.py
@@ -11,31 +11,36 @@ def test_future_events(client):
     # given
     expected_number_of_future_events = 3
     number_of_past_events = 4
-    FutureEventFactory.create_batch(expected_number_of_future_events)
+    expected_future_events = FutureEventFactory.create_batch(expected_number_of_future_events)
     PastEventFactory.create_batch(number_of_past_events)
 
     # when
     response = client.get(url.url_not_detail(url.EVENTS, 'future_events'))
-    number_of_future_events = len(json.loads(response.content))
+    future_events = list()
+
+    for x in json.loads(response.content):
+        future_events.append(x['id'])
 
     # then
-    assert number_of_future_events == expected_number_of_future_events
+    assert set(future_events) == {x.id for x in expected_future_events} and expected_number_of_future_events == len(
+        future_events)
 
 
 @pytest.mark.django_db
 def test_todays_events(client):
     # given
-    number_of_todays_events = 3
+    expected_number_of_todays_events = 3
     number_of_past_events = 4
     PastEventFactory.create_batch(number_of_past_events)
-    expected_todays_events = TodaysEventsFactory.create_batch(number_of_todays_events)
+    expected_todays_events = TodaysEventsFactory.create_batch(expected_number_of_todays_events)
 
     # when
     response = client.get(url.url_not_detail(url.EVENTS, 'todays_events'))
-    todays_events = set()
+    todays_events = list()
 
     for x in json.loads(response.content):
-        todays_events.add(x['id'])
+        todays_events.append(x['id'])
 
     # then
-    assert todays_events == {x.id for x in expected_todays_events}
+    assert set(todays_events) == {x.id for x in expected_todays_events} and expected_number_of_todays_events == len(
+        todays_events)
